e30da26567f8acca795e935ab2da448db3f6f373
Alisonlima <aurin7128@gmail.com>
2025-12-04 23:58:49 +0700

    first-commit

--- stats/stats-dto/src/main/java/ru/practicum/ewm/dto/EndpointHitDto.java
+++ stats/stats-dto/src/main/java/ru/practicum/ewm/dto/EndpointHitDto.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.PastOrPresent;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
