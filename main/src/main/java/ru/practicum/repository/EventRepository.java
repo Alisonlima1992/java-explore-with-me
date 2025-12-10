3f531a99be770922bf43caf0c0f7ee5467c480c6
Alisonlima <aurin7128@gmail.com>
2025-12-10 21:06:41 +0700

    Диплом часть 2 попытка 1

--- main/src/main/java/ru/practicum/repository/EventRepository.java
+++ main/src/main/java/ru/practicum/repository/EventRepository.java
@@ -40,14 +40,13 @@ public interface EventRepository extends JpaRepository<Event, Long> {
             Pageable pageable);
 
     @Query("SELECT e FROM Event e " +
-            "WHERE (e.state = 'PUBLISHED') " +
+            "WHERE e.state = 'PUBLISHED' " +
             "AND (:text IS NULL OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) " +
             "OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%'))) " +
             "AND (:categories IS NULL OR e.category.id IN :categories) " +
             "AND (:paid IS NULL OR e.paid = :paid) " +
             "AND (:rangeStart IS NULL OR e.eventDate >= :rangeStart) " +
-            "AND (:rangeEnd IS NULL OR e.eventDate <= :rangeEnd) " +
-            "ORDER BY e.eventDate ASC")
+            "AND (:rangeEnd IS NULL OR e.eventDate <= :rangeEnd)")
     Page<Event> findEventsByPublic(
             @Param("text") String text,
             @Param("categories") List<Long> categories,
