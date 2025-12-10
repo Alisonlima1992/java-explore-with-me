6394cbf96b400fbb5bbb81dab1200609a974ee31
Alisonlima <aurin7128@gmail.com>
2025-12-11 00:14:50 +0700

    Диплом часть 2 попытка 1

--- main/src/main/java/ru/practicum/repository/EventRepository.java
+++ main/src/main/java/ru/practicum/repository/EventRepository.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.practicum.model.Event;
-import ru.practicum.model.Event.EventState;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
